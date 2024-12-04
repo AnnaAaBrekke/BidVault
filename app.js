50d7952fe7c11442472dbe6f6a12ea20fb670ab2
AnnaAaBrekke <annabrekke98@gmail.com>
2024-12-04 16:57:01 +0100

    Added stylesheet to app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import "./src/styles/scss/styles.scss";
+import "./src/styles/css/styles.scss";
 
 import router from "./src/js/views/router.js";
 import { setLogoutListener } from "./src/js/global/logout.js";
