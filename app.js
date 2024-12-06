ec74fff57a3e1ebab216025d74a9bd2910b08418
AnnaAaBrekke <annabrekke98@gmail.com>
2024-12-06 10:04:20 +0100

    Removed link stylesheet in HTML and import injs

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-import "./src/styles/css/styles.scss";
-
 import router from "./src/js/views/router.js";
 import { setLogoutListener } from "./src/js/global/logout.js";
+import "./src/styles/css/style.css";
 
 router();
 setLogoutListener();
